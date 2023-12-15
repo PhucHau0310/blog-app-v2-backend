2527f678898433e74311d42736b73aba8fa08c22
Phuc Hau <haunhpr024@gmail.com>
2023-12-15 13:37:04 +0700

    error cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(
     cors({
         origin: 'http://localhost:3000', // Replace with the actual origin of your frontend
-        // credentials: true,
+        credentials: true,
     })
 );
 
